88f6e5787a95d228adba39e9cdc0cd01314e3524
Afi Adhikari <afiadhikari.01@gmail.com>
2018-10-05 22:28:46 +0530

    Add homepage

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $("#gre-panel").click(function() {
+        window.location.href = 'gre.html';
+    });
+
+    $("#gmat-panel").click(function() {
+        window.location.href = 'gmat.html';
+    });
+
+    $("#gate-panel").click(function() {
+        window.location.href = 'gate.html';
+    });
+
+    $("#upsc-panel").click(function() {
+        window.location.href = 'upsc.html';
+    });
+});
